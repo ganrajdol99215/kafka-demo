93ebd4cfd843afedfc6ecd6e814a4d0c25df7c8b
Ganraj Dol <84555910+ganrajdol99215@users.noreply.github.com>
2026-01-04 20:33:02 +0530

    Update EventConsumer.java

--- app/src/main/java/com/demo/kafkaui/kafka/EventConsumer.java
+++ app/src/main/java/com/demo/kafkaui/kafka/EventConsumer.java
@@ -1,3 +1,8 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
 @Component
 public class EventConsumer {
 
