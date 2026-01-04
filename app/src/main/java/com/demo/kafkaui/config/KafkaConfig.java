6f9994ccbd306f7ac5cd9a6fdc812670f845352c
Ganraj Dol <84555910+ganrajdol99215@users.noreply.github.com>
2026-01-04 20:34:13 +0530

    Update KafkaConfig.java

--- app/src/main/java/com/demo/kafkaui/config/KafkaConfig.java
+++ app/src/main/java/com/demo/kafkaui/config/KafkaConfig.java
@@ -1,3 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+
 @Configuration
 @EnableKafka
 public class KafkaConfig {
