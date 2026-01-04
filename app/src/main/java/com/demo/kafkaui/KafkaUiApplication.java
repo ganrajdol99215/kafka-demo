f63dd3c538dd4ba5f849508bb888a68d465a8950
Ganraj Dol <84555910+ganrajdol99215@users.noreply.github.com>
2026-01-04 21:18:39 +0530

    Update KafkaUiApplication.java

--- app/src/main/java/com/demo/kafkaui/KafkaUiApplication.java
+++ app/src/main/java/com/demo/kafkaui/KafkaUiApplication.java
@@ -1,8 +1,12 @@
+package com.demo.kafkaui;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = R2dbcAutoConfiguration.class)
 public class KafkaUiApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(KafkaUiApplication.class, args);
     }
