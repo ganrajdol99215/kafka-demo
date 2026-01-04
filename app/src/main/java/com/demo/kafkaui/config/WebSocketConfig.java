75deb11c59d7798b95fe5431b73caef77f1f24eb
Ganraj Dol <84555910+ganrajdol99215@users.noreply.github.com>
2026-01-04 19:23:48 +0530

    Create WebSocketConfig.java

---
+++ app/src/main/java/com/demo/kafkaui/config/WebSocketConfig.java
@@ -0,0 +1,15 @@
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
